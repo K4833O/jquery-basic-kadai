42ada7c0a9037fb14f568e0f493b507d6aa5f600
K4833O <142191146+K4833O@users.noreply.github.com>
2023-09-11 20:06:50 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,9 @@
+$(function() {
+  $(document).on('scroll', () => {
+      console.log('scrollイベントが発生しました');
+    });
+  });
+
+  $(window).on('load', () => {
+    console.log('loadイベントが発生しました');
+  });
